1f912e8cf1a5a8c7d673de9707453c2a8929336b
Macarena Reale Portelli <68354897+Macarealeportelli@users.noreply.github.com>
2022-06-15 09:42:58 -0300

    Trabajando con arrays

--- js/productos.js
+++ js/productos.js
@@ -1,4 +1,17 @@
-// let productos = []
+let productos = []
+let carrito = []
+let IVA = 1.21
+
+class Producto {
+    constructor(id, nombre, importe) {
+        this.id = id
+        this.nombre = nombre
+        this.importe = importe
+    }
+    precioFinal() {
+        return parseFloat((this.importe * IVA).toFixed(2))
+    }
+}
 
 function listarProductos() {
     console.table(productos)
@@ -41,18 +54,36 @@ function creoID() {
 generadorAutomatico()
 //FUNCION QUE SE USA PARA LLENAR AUTOMÁTICAMENTE EL ARRAY DE OBJETOS A USAR
 function generadorAutomatico() {
-    productos.push(new Producto(1234, "NOTEBOOK EXO E17", 29950))
-    productos.push(new Producto(2345, "MACBOOK AIR 13", 249900))
-    productos.push(new Producto(3456, "LENOVO IDEAPAD 13", 199949))
-    productos.push(new Producto(4567, "IPAD PRO 12", 219890))
-    productos.push(new Producto(5678, "LENOVO GAMER 15", 409090))
-    productos.push(new Producto(6789, "MACBOOK PRO 15", 459000))
-    productos.push(new Producto(7890, "ASUS GAMING PRO 17", 679800))
-    //productos.push(new Producto(7890, "IPAD MINI 7.9", 189900))
+    productos.push(new Producto(1234, "CUADERNO FLOWERS", 600))
+    productos.push(new Producto(2345, "AGENDA PICASSO", 2300))
+    productos.push(new Producto(3456, "COMBO UNIVERSITARIO", 3000))
+    productos.push(new Producto(4567, "COMBO PEDIÁTRICO", 2700))
+    productos.push(new Producto(5678, "COMBO OFICINA", 4050))
+    productos.push(new Producto(6789, "CUADERNO PINK", 1700))
+    productos.push(new Producto(7890, "AGENDA MOON", 1900))
+    productos.push(new Producto(7890, "AGENDA POCKET FLOWERS", 1400))
 }
 
 function generarCarrito() {
-    carrito.push(new Producto(1234, "NOTEBOOK EXO E17", 29950))
-    carrito.push(new Producto(2345, "MACBOOK AIR 13", 249900))
-    carrito.push(new Producto(3456, "LENOVO IDEAPAD 13", 199949))
+    carrito.push(new Producto(1234, "CUADERNO FLOWERS", 600))
+    carrito.push(new Producto(2345, "AGENDA PICASSO", 2300))
+    carrito.push(new Producto(3456, "COMBO UNIVERSITARIO", 3000))
+}
+
+let carritoPrecioFinal = []
+
+const pFCarrito = () => {
+    generarCarrito()
+    for(let item of carrito) {
+        precioFinal = item.precioFinal()
+        carritoPrecioFinal.push(precioFinal)
+    }
+    return carritoPrecioFinal
+}
+
+const calcularCarrito = () => {
+    pFCarrito()
+   let suma = carritoPrecioFinal.reduce((total, value) => total + value)
+      
+    return console.log("Precio Final de Cada Producto: " + carritoPrecioFinal, "Total del Carrito:" + suma)
 }
