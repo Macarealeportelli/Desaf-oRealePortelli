d445f3358d3fd20752e84783acdc28354db7d060
Macarena Reale Portelli <68354897+Macarealeportelli@users.noreply.github.com>
2022-06-22 16:32:57 -0300

    agregando eventos

--- js/carrito.js
+++ js/carrito.js
@@ -1,10 +1,12 @@
 import {  productos } from './stock.js'
 
 let carritoCompras = []
+let carritoPrecioFinal = []
 
 export const agregarACarrito = (productoId) => {
-   const contenedorCarrito = document.getElementById('carrito')
-    const renderProductoCarrito =()=>{
+    const contenedorCarrito = document.getElementById('carrito')
+    const btnCalcularCarrito = document.getElementById('btn-calcularCarrito')
+    const renderProductoCarrito = () => {
         let producto = productos.find( producto => producto.id == productoId )
         carritoCompras.push(producto)
 
@@ -14,11 +16,34 @@ export const agregarACarrito = (productoId) => {
         let div = document.createElement('div')
         div.classList.add('productoEnCarrito')
         div.innerHTML = `
-        <p>${producto.nombre}</p>
-        <p>Precio: ${producto.precio}</p> 
-        <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+        <p>${producto.name}</p>
+        <p>Precio: $ ${producto.price}</p> 
+        <p id="cantidad${producto.id}">Cantidad: 1 </p>
         <button id="eliminar${producto.id}" class="boton-eliminar" ><i class="fa-solid fa-trash-can"></i></button> `
         
         contenedorCarrito.appendChild(div)
     }
+    renderProductoCarrito()
+    console.table(carritoCompras)
+
+    // calculamos precio final de productos y carrito
+    // let precioFinal = (producto) => {
+    //     return parseFloat((producto.price * IVA).toFixed(2))
+    // }
+
+    // const pFCarrito = () => {
+    //     for(let item of carritoCompras) {
+    //         precioFinal = item.precioFinal()
+    //         carritoPrecioFinal.push(precioFinal)
+    //     }
+    //     return carritoPrecioFinal
+    // }
+    // btnCalcularCarrito.addEventListener('click', () => {
+    //     pFCarrito()
+    //     let suma = carritoPrecioFinal.reduce((total, value) => total + value)
+        
+    //     return console.log("Precio Final de Cada Producto: " + carritoPrecioFinal, "Total del Carrito:" + suma)
+    // })
 }
+
+
