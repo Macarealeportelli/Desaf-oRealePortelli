41896b0e510a245733df28756b23be9994fbeb92
Macarena Reale Portelli <68354897+Macarealeportelli@users.noreply.github.com>
2022-07-01 17:59:22 -0300

    utilizando LocalStorage y JSON

--- js/carrito.js
+++ js/carrito.js
@@ -3,13 +3,40 @@ import {  productos } from './stock.js'
 let carritoCompras = []
 let carritoPrecioFinal = []
 
+const guardoCarrito = () => {
+    if (carritoCompras.length > 0) {
+        localStorage.setItem("carrito", JSON.stringify(carritoCompras))
+    }
+}
+
+const recuperoCarrito = () => {
+    debugger
+    if (miCarrito = JSON.parse(localStorage.getItem("carrito"))){
+        miCarrito.forEach(producto => {
+            carritoCompras.push(producto)
+        })
+    }
+}
+recuperoCarrito()
+
+const removerDeCarrito = (producto) => {
+    const productoARemover = document.getElementById(`${producto.id}`)
+    productoARemover.remove()
+    item = carritoCompras.indexOf(productoARemover.innerText)
+    if (item >=0){
+        carritoCompras.splice(item, 1)
+        //guardoCarrito
+    }
+    console.warn(`${producto} ha sido eliminado del carrito`)
+}
+
 export const agregarACarrito = (productoId) => {
     const contenedorCarrito = document.getElementById('carrito')
     const btnCalcularCarrito = document.getElementById('btn-calcularCarrito')
     const renderProductoCarrito = () => {
         let producto = productos.find( producto => producto.id == productoId )
         carritoCompras.push(producto)
-
+            
         producto.cantidad = 1
         //condicional para q vea la cantidad de productos
 
@@ -25,9 +52,10 @@ export const agregarACarrito = (productoId) => {
         contenedorCarrito.appendChild(div)
     }
     renderProductoCarrito()
+    guardoCarrito()
     console.table(carritoCompras)
 
-    // calculamos precio final de productos y carrito
+      // calculamos precio final de productos y carrito
     // let precioFinal = (producto) => {
     //     return parseFloat((producto.price * IVA).toFixed(2))
     // }
@@ -46,5 +74,4 @@ export const agregarACarrito = (productoId) => {
     //     return console.log("Precio Final de Cada Producto: " + carritoPrecioFinal, "Total del Carrito:" + suma)
     // })
 }
-
 
---
+++ js/checkout.js
@@ -0,0 +1,45 @@
+const inputNombre = document.querySelector("#inputNombre")
+const inputApellido = document.querySelector("#inputApellido")
+const inputTelefono = document.querySelector("#inputPhone")
+const inputEmail = document.querySelector("#inputEmail")
+const btnSubmit = document.querySelector("#submit")
+
+let datosDeInput = ""
+
+document.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    guardarDatosUser()
+    alert(`Gracias por su compra &#128521`)
+})
+
+const guardarDatosUser = () => {
+    // debugger
+    const datosUser = {
+        nombre: inputNombre.value,
+        apellido: inputApellido.value,
+        email: inputEmail.value,
+        telefono: inputTelefono.value
+    }
+    let str = JSON.stringify(datosUser)
+    localStorage.setItem("datosUser", str)
+}
+
+const recuperarDatosUser = () => {
+    if (localStorage.getItem("datosUser")){
+        const datosUser = JSON.parse(localStorage.getItem("datosUser"))
+        inputNombre.value = datosUser.nombre
+        inputApellido.value = datosUser.apellido
+        inputEmail.value = datosUser.email
+        inputTelefono.value = datosUser.telefono
+    }
+    const carrito = JSON.parse(localStorage.getItem("carrito"))
+    // for (let i = 0 ; i< carrito.length ; i++) {
+    //     document.querySelector("#productosComprados").innerText = carrito[i].name.join(" - ")
+    // }
+
+// para q esta parte funcione deberia recorrer el carrito y mostrar cada uno de los elementos,
+// pero como es un objeto no me sale como desglosarlo :/
+    document.querySelector("#productosComprados").innerText = carrito.join(" - ")
+}
+
+recuperarDatosUser()
